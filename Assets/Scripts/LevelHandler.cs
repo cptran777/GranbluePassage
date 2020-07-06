ae37686652c15a5ee4145d0eb57d6cacf2b9324a
Charlie Tran <cptran14@gmail.com>
2020-07-06 14:32:23 -0700

    Add text mesh pro, init game over screen, and add enemy animations

---
+++ Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ * Manages the transitions between levels
+ */
+public class LevelHandler : MonoBehaviour {
+    public void LoadGameOver() {
+        SceneManager.LoadScene("Game Over");
+    }
+}
