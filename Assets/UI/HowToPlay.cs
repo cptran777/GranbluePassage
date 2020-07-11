8a7c4b4e0deb172a998fc437f89f6695a873716f
Charlie Tran <cptran14@gmail.com>
2020-07-10 21:22:25 -0700

    Add instructions on how to play to homescreen

---
+++ Assets/UI/HowToPlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlay : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    public void ClosePlayInstructions() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/UI/TriggerPlayInstructions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerPlayInstructions : MonoBehaviour {
+
+    [SerializeField] GameObject playInstructionsObject;
+
+    [SerializeField] Canvas canvasElement;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    public void OpenPlayInstructions() {
+        if (FindObjectsOfType<HowToPlay>().Length < 1) {
+            GameObject playInstructions = Instantiate(playInstructionsObject);
+            playInstructions.transform.SetParent(canvasElement.transform);
+            RectTransform rectTransform = playInstructions.GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector2(0, 0);
+        }
+    }
+}
